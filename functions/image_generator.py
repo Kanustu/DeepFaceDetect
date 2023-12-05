94af2c980080db60e33f2189b1fd449fcd83a834
JDKanius <kanustu@gmail.com>
2023-12-04 19:45:08 -0600

    04/12 function notebok

--- functions/image_generator.py
+++ functions/image_generator.py
@@ -1,6 +1,6 @@
-def image_generator(tr):
+def image_generator(state):
     image_gen.flow_from_directory(
-    base_path + 'train/',
+    base_path + f'{state}/',
     target_size=(224, 224),
     batch_size=64,
     class_mode='binary'
