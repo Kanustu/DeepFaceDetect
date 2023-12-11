6ad5948c54ed2f522535ce55e46edce8c316a9d1
JDKanius <kanustu@gmail.com>
2023-12-11 13:41:29 -0600

    modified app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,24 @@
 import streamlit as st
 
 st.title('DeepFakeGuard: Real or Fake')
+st.divider()
 
-st.sidebar.title('About')
+st.sidebar.title('Something')
 
-st.file_uploader('Upload your images')
+
+st.divider()
+
+tab1, tab2, tab3 = st.tabs(['Xception', 'VGG16', 'ResNet50'])
+
+with tab1:
+    st.header("Xception")
+    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+    st.file_uploader('Upload your images')
+with tab2:
+    st.header("VGG16")
+    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+    st.file_uploader('Upload your images')
+with tab3:
+    st.header("ResNet50")
+    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+    st.file_uploader('Upload your images')
