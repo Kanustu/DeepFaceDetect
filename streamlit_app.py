d3173fd1cfba4e9ecf1f125e74cdc7d425d16c9f
JDKanius <kanustu@gmail.com>
2024-01-01 17:43:59 -0600

    added model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import tensorflow as tf
 import keras
+from keras.models import load_model
 import pickle
 import pandas as pd
 st.title('DeepFakeGuard: Real or Fake')
-
+with open("models/Xception.pkl", 'rb') as f:
+            Xception = pickle.load(f)
 
 st.sidebar.file_uploader("Choose a file to upload")
 
@@ -38,7 +40,7 @@ with tab2:
     col4.metric(label="F-1 Score", value=88, delta=f'{88-50}*')
     st.caption('*=compared to _Baseline_')
     st.divider()
-    st.image("confusion_tables/confusion_Xception.png")
+    st.image("/confusion_tables/confusion_Xception.png")
     st.divider()
 
 
