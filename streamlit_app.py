4cc628f0a0b92053a7697d57dbe4ceddd7feab43
JDKanius <kanustu@gmail.com>
2024-01-02 09:01:25 -0600

    added model

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from keras.models import load_model
 import pickle
 import pandas as pd
 
-loaded_model=pickle.load(open("models/Xception.pkl",'rb'))
+#loaded_model=pickle.load(open("models/Xception.pkl",'rb'))
 
 st.title('DeepFakeGuard: Real or Fake')
 
