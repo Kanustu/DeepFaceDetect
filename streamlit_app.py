1f17a0d55255804b68a1a49ec77140c734652bce
JDKanius <kanustu@gmail.com>
2024-01-02 07:49:58 -0600

    fixed model

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from keras.models import load_model
 import pickle
 import pandas as pd
 st.title('DeepFakeGuard: Real or Fake')
-with open("../models/Xception.pkl", "rb") as f:
+with open("models/Xception.pkl", "rb") as f:
             loaded_Xception = pickle.load(f)
 
 st.sidebar.file_uploader("Choose a file to upload")
