dec75d139eedea36ae6c21907d1db3888cad453f
JDKanius <kanustu@gmail.com>
2024-01-09 09:34:10 -0600

    updated notebooks

--- Notebooks/functions/.ipynb_checkpoints/color_test-checkpoint.py
+++ Notebooks/functions/.ipynb_checkpoints/color_test-checkpoint.py
@@ -1,19 +1,59 @@
 def color_info(path):
+    """
+    Extracts color information (bands) from a list of image files.
+
+    Parameters:
+    - path (list): List of file paths to image files.
+
+    Returns:
+    - list: A list containing color information (bands) for each image in the input list.
+    """
     from PIL import Image
+
+    # Initialize an empty list to store color information for each image
     color_list = []
+
+    # Iterate through the list of image file paths
     for x in range(len(path)):
+        # Open each image and retrieve its color information (bands)
         color_list.append(Image.open(f'../real_vs_fake/real-vs-fake/{path[x]}').getbands())
+
     return color_list
 
 
-def color_test(list):
+
+def color_test(color_info_list):
+    """
+    Filters a list of color information to include only RGB images.
+
+    Parameters:
+    - color_info_list (list): List of color information for images.
+
+    Returns:
+    - list: A filtered list containing only color information for RGB images (R, G, B).
+    """
     color_list = []
-    for x in list:
+
+    # Iterate through the list of color information for images
+    for x in color_info_list:
+        # Check if the color information corresponds to an RGB image (R, G, B)
         if x == ('R', 'G', 'B'):
             color_list.append(x)
+
     return color_list
 
+
 def length_compare(var1, var2):
+    """
+    Compares the lengths of two variables.
+
+    Parameters:
+    - var1: First variable for length comparison.
+    - var2: Second variable for length comparison.
+
+    Returns:
+    - bool: True if the lengths of var1 and var2 are equal, False otherwise.
+    """
     if len(var1) == len(var2):
         return True
     else:
--- Notebooks/functions/.ipynb_checkpoints/visualize-checkpoint.py
+++ Notebooks/functions/.ipynb_checkpoints/visualize-checkpoint.py
@@ -100,20 +100,57 @@ def multi_histplot(df1, df2, df3):
         sns.histplot(data=df_list[x], x='label_str', ax=axes[x]).set(title=title_list[x])
 
 def multi_scatter():
+    """
+    Create a multi-scatter plot showing the relationship between image size and set.
+
+    This function uses matplotlib and seaborn to generate subplots for each set, displaying scatter plots
+    of image size distribution. The x_values and y_values represent image width and height, respectively,
+    for the train, valid, and test sets.
+
+    Returns:
+    - None
+    """
+    # Import necessary libraries
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    # Create subplots for a multi-scatter plot
     fig, axes = plt.subplots(1, 3, figsize=(8, 4))
     fig.subplots_adjust(hspace=0.30, wspace=0.5)
     fig.suptitle('Image Size By Set')
-    title_list=['train_set','valid_set','test_set']
-    axis_list = [(0,0),(0,1),(0,2)]
+
+    # Define title and axis information
+    title_list = ['train_set', 'valid_set', 'test_set']
     x_values = [train_x, valid_x, test_x]
     y_values = [train_y, valid_y, test_y]
+
+    # Iterate through the sets and create scatter plots
     for x in range(3):
-        sns.scatterplot(x=x_values[x], y=y_values[x],ax=axes[x]).set(title=title_list[x])
-    
+        sns.scatterplot(x=x_values[x], y=y_values[x], ax=axes[x]).set(title=title_list[x])
+
 def color_hist(image):
+    """
+    Generate a histogram for each color channel in the input image.
+
+    This function uses NumPy and Matplotlib to create separate histograms for each color channel
+    (Red, Green, and Blue) in the input image. The histograms illustrate the distribution of pixel
+    intensities for each color channel.
+
+    Parameters:
+    - image (PIL.Image.Image): The input image for which histograms will be generated.
+
+    Returns:
+    - None
+    """
     import numpy as np
     import matplotlib.pyplot as plt
+
+    # Convert the PIL Image to a NumPy array
     array = np.array(image)
+
+    # Create histograms for each color channel
     for x in range(3):
         plt.hist(array[:, :, x].ravel(), bins=256, color=f'C{x}', alpha=0.6)
+
+    # Display the histograms
     plt.show()
